40667e11244de90b67ff34d223f99b82dee78208
Katy St Sauveur <katyrogowski@yahoo.com>
2020-06-10 20:25:31 -0700

    Clean player class

--- src/player.js
+++ src/player.js
@@ -3,30 +3,14 @@ class Player {
     this.id = Date.now();
     this.wins = 0;
     this.hand = [];
-
-    // this.playerTwo = new Player();
-    // console.log('hand', this.hand)
-
   }
+
   playCard() {
-    // console.log('hand', this.hand)
     var card = this.hand.shift()
     gameOne.fullDeck.push(card);
-    // gameOne.fullDeck.length = 0;
-    console.log('card', card)
-    // console.log('centerDeck', centerDeck);
-    //player one flips a card
-    //it is added to the middle pile array
-    //it is removed from playerOne hand array
-    //player two flips a card
-    //it is added to the middle pile array
-    //it is removed from playerTwo hand array
   }
-  saveWinsToStorage() {
 
-  }
   winGame() {
-    console.log('player wins')
     this.wins++;
   }
-}
+};
